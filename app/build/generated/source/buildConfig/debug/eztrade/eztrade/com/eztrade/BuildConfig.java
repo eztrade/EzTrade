e93a06a64c029f799060c6893b19ab36beaf9248
Anand KumarYog <anandkyog@gmail.com>
2015-01-11 22:38:55 -0500

    Initial commit of extrude android application. This commit contains two screens - welcome and sell skeletons

---
+++ app/build/generated/source/buildConfig/debug/eztrade/eztrade/com/eztrade/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package eztrade.eztrade.com.eztrade;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "eztrade.eztrade.com.eztrade";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
