07e8625384adf631aaa19fd03d0d376a3a3b8664
eztrade <nitinreddyb@gmail.com>
2015-02-10 23:26:03 -0500

    added keyword search to buyer page

--- app/src/main/java/com/eztrade/eztrade/Activities/BuyQueryActivity.java
+++ app/src/main/java/com/eztrade/eztrade/Activities/BuyQueryActivity.java
@@ -6,34 +6,41 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import eztrade.eztrade.com.eztrade.R;
 
 public class BuyQueryActivity extends Activity {
+    ListView resultsList;
+    EditText keyword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_query);
-        EditText keyword = (EditText) findViewById(R.id.keywordET);
+        keyword = (EditText) findViewById(R.id.keywordET);
         EditText zip = (EditText) findViewById(R.id.zipET);
         Button searchBtn = (Button) findViewById(R.id.searchBtn);   
-        ListView resultsList = (ListView) findViewById(R.id.resultsList);
+        resultsList = (ListView) findViewById(R.id.resultsList);
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ParseQuery<ParseObject> query = ParseQuery.getQuery("Post");
+                query.whereContains("description", keyword.getText().toString());
                 query.findInBackground(new FindCallback<ParseObject>() {
                     public void done(List<ParseObject> objects, ParseException e) {
                         if (e == null) {
@@ -49,7 +56,15 @@ public class BuyQueryActivity extends Activity {
     }
 
     private void objectsWereRetrievedSuccessfully(List<ParseObject> objects) {
+        ArrayList<String> posts = new ArrayList<>();
+        for(ParseObject post : objects)
+        {
+            posts.add((String)post.get("title"));
+        }
+        String[] postArr = new String[posts.size()];
+        ArrayAdapter<String> listadapter = new ArrayAdapter<String>(this,R.layout.list_item_layout,posts.toArray(postArr));
 
+        resultsList.setAdapter(listadapter);
     }
 
 
